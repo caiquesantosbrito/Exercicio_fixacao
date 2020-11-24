3d9b9031ecc7a8908f8865339d75035d3b56eef8
Caique Santos <caisbrito2003@gmail.com>
2020-11-24 20:47:10 -0300

    Exer 1 feito

---
+++ Exercício 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercício_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o ano atual: ");
+            int ano_atual = int.Parse(Console.ReadLine());
+            Console.WriteLine("----------------------------------");
+            Console.Write("Digite o ano em que nasceu: ");
+            int ano_que_nasceu = int.Parse(Console.ReadLine());
+            Console.WriteLine("----------------------------------");
+
+            int anos = ano_atual - ano_que_nasceu;
+
+            if(anos >= 18){
+                Console.WriteLine("Você pode votar!");
+            }
+            else{
+                Console.WriteLine("Você ainda não tem idade para votar!");
+            }
+        }
+    }
+}
