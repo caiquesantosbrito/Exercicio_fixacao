a0b6f27bb337ce1b5a9694b772b273d5ab8900b3
Caique Santos <caisbrito2003@gmail.com>
2020-11-25 14:50:15 -0300

    Exer 4 feito

---
+++ Exercício 4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercício_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[10];
+            int menor = 0, maior = 0;
+
+            Console.WriteLine( "Insira 10 número:" );
+
+            for(int i = 0; i < 10; i++){
+
+                Console.Write( ( i + 1 ) + "º número:");
+                num[i] = int.Parse(Console.ReadLine());
+
+                if(i == 0){
+                    menor = num[0];
+                    maior = num[0];
+                }
+                if(num[i] < menor){
+                    menor = num[i];
+                }
+                else if(num[i] > maior){
+                    maior = num[i];
+                }
+            }
+            Console.WriteLine( "O menor número é: " + menor );
+            Console.WriteLine( "O maior número é: " + maior );
+        }
+    }
+}
