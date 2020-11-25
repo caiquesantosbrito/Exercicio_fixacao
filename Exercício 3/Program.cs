2fc4202c282ad62a14686d15094100d52f38c3a2
Caique Santos <caisbrito2003@gmail.com>
2020-11-24 21:23:55 -0300

    Exer 3 feito

---
+++ Exercício 3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Exercício_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float discount1 = 0.02f;
+            float discount2 = 0.03f;
+            float discount3 = 0.05f;
+
+            Console.WriteLine("Digite o nome do produto que você comprou:");
+            string produto = Console.ReadLine();
+            Console.WriteLine("Digite quantas unidades você comprou:");
+            int unidades = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor unitário do produto:");
+            float preco = float.Parse(Console.ReadLine());
+
+            if(unidades <= 5){
+                float total = (unidades * preco);
+                float desconto = (total * discount1);
+                float final = (total - desconto);
+
+                Console.WriteLine($"O preço final a se pagar com o desconto é de {final}");
+
+            }else if(unidades >5 || unidades <=10){
+                float total = (unidades * preco);
+                float desconto = (total * discount2);
+                float final = (total - desconto);
+
+                Console.WriteLine($"O preço final a se pagar com o desconto é de {final}");
+                
+            }else{
+                float total = (unidades * preco);
+                float desconto = (total * discount3);
+                float final = (total - desconto);
+
+                Console.WriteLine($"O preço final a se pagar com o desconto é de {final}");
+            }
+        }
+    }
+}
