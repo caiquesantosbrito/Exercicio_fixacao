ef8d2ee799d75e44497700f9a3b7e895d7fadc90
Caique Santos <caisbrito2003@gmail.com>
2020-11-25 15:32:06 -0300

    Exer 5 feito

---
+++ Exercício 5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercício_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira o número da tabuada que deseja ver:");
+            int resposta = int.Parse(Console.ReadLine());
+            Console.WriteLine("---------------------------------------------------");
+
+            int resultado;
+
+            for (int contador = 10; contador > 0; contador--){
+            
+                resultado = resposta * contador;
+                Console.WriteLine($"{resposta} * {contador} = {resultado}");
+                System.Threading.Thread.Sleep(700);
+            }
+        }
+    }
+}
