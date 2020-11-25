6badc3dba30b63588064fdb36733f27a0d187be9
Caique Santos <caisbrito2003@gmail.com>
2020-11-25 17:20:26 -0300

    Exer 7 feito

---
+++ Exercício 7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercício_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[16];
+            Console.WriteLine("Digite 15 números");
+            
+            for (int i = 1; i < 16; i++){
+               Console.Write($"Número {i}: ");
+               numeros[i] = int.Parse(Console.ReadLine());
+            }
+            for (int a = 15; a > 0; a--){
+                Console.Write($"{numeros[a]}, ");
+            }
+        }
+    }
+}
